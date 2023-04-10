c649b5cc9c182627050a6081448a39ca6d502fe0
A Mouginot <mouginot.arthur@gmail.com>
2023-04-10 18:05:15 +0200

    major speed update of scytale, much faster

--- scytale/scytale.py
+++ scytale/scytale.py
@@ -2,16 +2,11 @@
 
 from PIL import Image as Img
 import os
-import time #
-import random
 
-folderPath = os.getcwd() # For mac
+folderPath = os.getcwd() + '/arthurPython' # For mac
 #folderPath = 'P:\Documents\py' # For windows, the path to the program's folder
 
 chars = [' ', '!', '“', '"', '#', '$', '%', '&', '‘', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '÷', '¡', '¢', '£', '€', '¤', '¥', '¦', '§', '¨', '©', 'ª', '«', '¬', '®', '¯', '°', '±', '²', '³', '´', 'µ', '¶', '·', '¸', '¹', 'º', '»', '¿', 'À', 'Á', ' ', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', '×', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷', 'Б', 'Г', 'Д', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'П', 'У', 'Ф', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'б', 'в', 'г', 'д', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'п', 'т', 'у', 'ф', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'Δ', 'Θ', 'Λ', 'Ξ', 'Π', 'Σ', 'Φ', 'Ψ', 'Ω', 'α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'λ', 'μ', 'ν', 'ξ', 'π', 'ρ', 'ς', 'σ', 'φ', 'ψ', 'ω']
-
-def splitString(string):
-    return [char for char in string]
 
 #img
 
@@ -57,90 +52,68 @@ def imgToNums(img):
             for i in range(3):   
                 nums.append(pxl[i])
     return(nums)
-    
-def generateRandomStableKey(length):
-    timer1 = time.time() #
-    stableKey, idx, plugboard = [], 0, [i for i in range(256)]
-    for i in range(length):
-        if idx < 256:
-            num = random.choice(plugboard)
-            plugboard.pop(plugboard.index(num))
-            stableKey.append(num)
-        else:
-            stableKey.append(random.randrange(256))
-        idx += 1
-        if idx > 257:
-            idx, plugboard = 0, [i for i in range(256)]
-    while idx != 258:
-        if idx < 256:
-            num = random.choice(plugboard)
-            plugboard.pop(plugboard.index(num))
-            stableKey.append(num)
-        else:
-            stableKey.append(random.randrange(256))
-        idx += 1    
-    print('stabilizing total time ' + str(time.time() - timer1)) #
-    return(stableKey)
-    
+        
 def stabilizeKey(key):
-    timer1 = time.time() #
-    idx, plugboard = 0, []
-    for i in range(len(key)):
-        if idx < 256:
-            while key[i] in plugboard:
-                key[i] += 1
-                key[i] %= 256
-            plugboard.append(key[i])
-        idx += 1
-        if idx > 257:
-            idx, plugboard = 0, []
-    while idx != 258:
-        if idx < 256:
-            replacementNum = 0
-            while replacementNum in plugboard:
-                replacementNum += 1
-            plugboard.append(replacementNum)
-            key.append(replacementNum)
-        else:
-            key.append(1)
-        idx += 1    
+    keySet = set(key[0:255])
+    for i in range(256):
+        if not i in keySet:
+            keySet.add(i)
+    return([list(keySet), [key[256], key[257]]])
     
-    print('stabilizing total time ' + str(time.time() - timer1)) #
-    return(key)
 
-def messageToNums(characters, message):
-    pass
+def messageToNums(charactersList, message):
+    for idx, i in enumerate(message):
+        message[idx] = charactersList.index(i)
+    return(message)
+    
+def numsToMessage(charactersList, message):
+    for idx, i in enumerate(message):
+        message[idx] = charactersList[i]
+    return(message)
 
-def rotor(character, rotorSetting):
-    pass
+def rotor(character, rotorSetting, type):
+    if type == 'encode':
+        character += rotorSetting
+    else:
+        character += 256 - rotorSetting
+    character %= 256
+    return(character)
 
-def plugboard(character, plugboardSetting):
-    pass
+def plugboard(character, plugboardSetting, type):
+    if type == 'encode':
+        return(plugboardSetting[character])
+    else:
+        return(plugboardSetting.index(character))
 
 def mirror(character):
-    pass
+    return(255 - character)
 
-def scytale(message, settings):
-    pass
-
-#to do: change variable names (foo), remove all "pass"
-
-print('step1')
-new = getImg(folderPath, 'cassini.png')
-print('step2')
-nums = imgToNums(new)
-print('step3')
-key = stabilizeKey(nums)
-print('step4')
-saveImg(numsToImg(key, new.size), folderPath, 'image17.png')
+def scytale(operation, characters, message, imageNumbers):
+    messageList = [char for char in message]
+    messageNums = messageToNums(characters, messageList)
+    possibleSetsInImgNums = len(imageNumbers) % 258
+    if operation == 'encode':
+        rotorChange = 0.5
+    else:
+        rotorChange = -0.5
+    for idx in range(len(messageNums)):
+        setNumber = idx % possibleSetsInImgNums
+        stableKey = stabilizeKey(imageNumbers[(setNumber * 258):((setNumber * 258) + 258)])
+        
+        messageNums[idx] = plugboard(messageNums[idx], stableKey[0], operation)
+        messageNums[idx] = rotor(messageNums[idx], stableKey[1][int(0.5 - rotorChange)], operation)
+        messageNums[idx] = mirror(messageNums[idx])
+        messageNums[idx] = rotor(messageNums[idx], stableKey[1][int(0.5 + rotorChange)], operation)
+        messageNums[idx] = plugboard(messageNums[idx], stableKey[0], operation)
+    return(''.join(numsToMessage(characters, messageNums)))
+    
+myText = 'en-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the the'
+img1Nums = imgToNums(getImg(folderPath, 'image1.png'))
+a = scytale('encode', chars, myText, img1Nums)
+print(a)
+print(scytale('decode', chars, a, img1Nums))
 
 # 86 batches of 3 bytes (8 bits) -> first 256 bytes = plugboard, byte 257 = rotor 1, byte 258 = rotor 2
 # plugboard -> rotor 1 -> mirror -> rotor 2 -> plugboard
 
 # to do: Julia? https://medium.com/@alienrobot/part-1-julia-images-moving-from-python-to-julia-image-i-o-basic-manipulations-eecae73fe04d
-
-#trial 1 : 34.75253939628601 s
-#trial 2 : 32.189963817596436 s
-#trial 3 : 32.3187370300293 s
-#trial 4 : 32.37806963920593 s
-#trial 5 : 20.388001918792725 s
