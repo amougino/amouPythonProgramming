d0b99c96ba9d446be585960a034b95525bd7fb01
A Mouginot <mouginot.arthur@gmail.com>
2024-08-01 18:48:49 +0200

    started pimo + gravsim imgs cleaned

---
+++ PiMo/PiMo.py
@@ -0,0 +1,50 @@
+import os
+import PIL.Image as img
+
+#all possible characters for strings (here the first object has a value of 1 and the last a value of 254)
+chars = [' ', '!', '“', '"', '#', '$', '%', '&', '‘', "'", '(', ')', '*', '+', ',', '-', 
+         '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', 
+         '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', 
+         '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', 
+         '~', '÷', '¡', '¢', '£', '€', '¤', '¥', '¦', '§', '¨', '©', 'ª', '«', '¬', '®', 
+         '¯', '°', '±', '²', '³', '´', 'µ', '¶', '·', '¸', '¹', 'º', '»', '¿', 'À', 'Á',
+         'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 
+         'Ô', 'Õ', 'Ö', '×', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'à', 'á', 'â', 'ã', 
+         'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 
+         'õ', 'ö', 'Б', 'Г', 'Д', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'П', 'У', 'Ф', 'Ц', 'Ч', 
+         'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'б', 'в', 'г', 'д', 'ж', 'з', 'и', 'й', 
+         'к', 'л', 'м', 'н', 'п', 'т', 'у', 'ф', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 
+         'ю', 'я', 'Δ', 'Θ', 'Λ', 'Ξ', 'Π', 'Σ', 'Φ', 'Ψ', 'Ω', 'α', 'β', 'γ', 'δ', 'ε', 
+         'ζ', 'η', 'θ', 'λ', 'μ', 'ν', 'ξ', 'π', 'ρ', 'ς', 'σ', 'φ', 'ψ', 'ω']
+
+def getImage(filePath = ''):
+    file = os.path.expanduser(filePath)
+    if file == '':
+        raise(Exception('File not defined'))
+    try:
+        image = img.open(file)
+        return(image)
+    except FileNotFoundError:
+        raise(Exception('File does not exist'))
+    
+class cursor:
+
+    def __init__(self, image, startingPosition = (0,0), direction = (1,0)):
+        self.image = image
+        self.imageSize = image.size
+        self.position = startingPosition
+        self.direction = direction
+
+    def checkForWalls(self):
+        pass
+    
+    def move(self):
+        pass
+
+    def readCell(self):
+        pass
+
+if __name__ == '__main__':
+    cursor = cursor(getImage(os.getcwd()+'/PiMo/PiMoImage1.png'))
---
+++ PiMo/createPiMoImage.py
@@ -0,0 +1,17 @@
+import PIL.Image as img
+import os
+
+def createImg(size):
+    createdImage = img.new('RGB', size, 'white')
+    return(createdImage)
+
+def saveImage(imageToSave, path):
+    file = os.path.expanduser(path)
+    imageToSave.save(file)
+
+PiMoImage = createImg((10,10))
+blackPixels = [(3,0),(2,3),(1,1),(0,2)]
+for pixel in blackPixels:
+    PiMoImage.putpixel(pixel,(0,0,0))
+
+saveImage(PiMoImage,os.getcwd()+'/PiMo/PiMoImage1.png')
