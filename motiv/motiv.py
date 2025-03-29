2c7098c63768c80b3044ad45d8f9df1eb389b0ff
A Mouginot <mouginot.arthur@gmail.com>
2025-03-29 18:20:36 +0100

    changed letters

--- motiv/motiv.py
+++ motiv/motiv.py
@@ -11,26 +11,26 @@ files = {
 format_replacements = [('[RRI]', '\n\n\t')]
 
 prepas = [
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Pierre De Fermat')], 'save_name' : 'mp2i_PDeFermat'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Joffre')], 'save_name' : 'mp2i_Joffre'},
-    {'sections' : ['P1','P2.1','P3.1','P4_1','P4_2.1','P5.2','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Champollion')], 'save_name' : 'mp2i_Champollion'},
-    {'sections' : ['P1','P2.2','P3.1','P4_1','P4_2.1','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'du Parc')], 'save_name' : 'mp2i_Parc'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'mp2i_MartiniereM'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.2','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Berthollet')], 'save_name' : 'mp2i_Berthollet'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'mp2i_LLG'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'mp2i_StLouis'},
-    {'sections' : ['P1','P2.1','P3.1','P4_1','P4_2.1','P5.2','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Champollion')], 'save_name' : 'mpsi_Champollion'},
-    {'sections' : ['P1','P2.2','P3.1','P4_1','P4_2.1','P5.1','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'du Parc')], 'save_name' : 'mpsi_Parc'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'mpsi_MartiniereM'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.2','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Berthollet')], 'save_name' : 'mpsi_Berthollet'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'mpsi_LLG'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'mpsi_StLouis'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Joffre')], 'save_name' : 'pcsi_Joffre'},
-    {'sections' : ['P1','P2.1','P3.1','P4_1','P4_2.1','P5.2','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Champollion')], 'save_name' : 'pcsi_Champollion'},
-    {'sections' : ['P1','P2.2','P3.1','P4_1','P4_2.1','P5.1','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'du Parc')], 'save_name' : 'pcsi_Parc'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'pcsi_MartiniereM'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'pcsi_LLG'},
-    {'sections' : ['P1','P2.1','P3.2','P4_1','P4_2.2','P5.1','P6','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'pcsi_StLouis'}
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Pierre De Fermat')], 'save_name' : 'mp2i_PDeFermat'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Joffre')], 'save_name' : 'mp2i_Joffre'},
+    {'sections' : ['P1','P2.1','P3.1','P4','P5_1','P5_2.2','P6.2','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Champollion')], 'save_name' : 'mp2i_Champollion'},
+    {'sections' : ['P1','P2.2','P3.1','P4','P5_1','P5_2.2','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'du Parc')], 'save_name' : 'mp2i_Parc'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'mp2i_MartiniereM'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.2','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Berthollet')], 'save_name' : 'mp2i_Berthollet'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'mp2i_LLG'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MP2I'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'mp2i_StLouis'},
+    {'sections' : ['P1','P2.1','P3.1','P4','P5_1','P5_2.2','P6.2','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Champollion')], 'save_name' : 'mpsi_Champollion'},
+    {'sections' : ['P1','P2.2','P3.1','P4','P5_1','P5_2.2','P6.1','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'du Parc')], 'save_name' : 'mpsi_Parc'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'mpsi_MartiniereM'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.2','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Berthollet')], 'save_name' : 'mpsi_Berthollet'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'mpsi_LLG'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'MPSI'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'mpsi_StLouis'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Joffre')], 'save_name' : 'pcsi_Joffre'},
+    {'sections' : ['P1','P2.1','P3.1','P4','P5_1','P5_2.2','P6.2','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Champollion')], 'save_name' : 'pcsi_Champollion'},
+    {'sections' : ['P1','P2.2','P3.1','P4','P5_1','P5_2.2','P6.1','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'du Parc')], 'save_name' : 'pcsi_Parc'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'La Martinière Monplaisir')], 'save_name' : 'pcsi_MartiniereM'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Louis Le Grand')], 'save_name' : 'pcsi_LLG'},
+    {'sections' : ['P1','P2.1','P3.2','P4','P5_1','P5_2.3','P6.1','P7'], 'replacements' : [('[type]', 'PCSI'), ('[lycee]', 'Saint-Louis')], 'save_name' : 'pcsi_StLouis'}
     ]
 
 def create_letter(format,parts):
@@ -40,10 +40,8 @@ def create_letter(format,parts):
         all_sections = [section.split('\n')[0].split('---') for section in lines]
         letter = ''
         for section in parts['sections']:
-            print(section)
             for section2 in all_sections:
                 if section == section2[0]:
-                    print(section2[0])
                     section_to_add = section2[1]
                     for replacement in format_replacements + parts['replacements']:
                         section_to_add = section_to_add.replace(replacement[0],replacement[1])
@@ -60,6 +58,7 @@ def wordcount(text,parts):
     new_text = text.replace('\n','')
     new_text = new_text.replace('\t','$')
     length = len(new_text)
+    print(length,parts['replacements'][0][1],parts['replacements'][1][1])
     if length > 1500:
         print('!!! WORDCOUNT TO HIGH !!! ' + parts['save_name'] + ' !!! ' + str(length) + ' characters !!!')
 
